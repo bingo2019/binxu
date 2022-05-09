6a7f8cb2b20f0bc46ba433c45e57754958c6573d
bingo2019 <274569314@qq.com>
2022-05-09 15:22:53 +0800

    2022.5.9 add week1.c

---
+++ week1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+//#include "stdlib.h"
+//#include "string.h"
+int main(int argc, char *argv[])
+{
+    printf("Test 1-1\n");   
+    printf("Hello World!\n"); //
+
+    printf("\nTest 1-2\n");   
+    printf("Welcom to you!\n");
+    
+    printf("\nTest 1-3\n");   
+    printf("Programming C is fun!\n");
+
+    printf("\nTest 1-4\n");   
+    printf("****\n");
+    printf("***\n");
+    printf("**\n");
+    printf("*\n");
+
+    printf("\nTest 1-5\n");   
+    printf(" A \n");
+    printf("A A\n");
+    printf(" A \n");
+
+    printf("\nTest 1-6\n");
+    printf("************\n");
+    printf("  Welcome   \n");
+    printf("************\n");
+
+    printf("\nTest 1-7\n");
+    printf("What is a computer?\n");
+
+    printf("\nTest 1-8\n");
+    printf("* * * *\n");
+    printf(" * * *\n");
+    printf("  * *\n");
+    printf("   *\n");
+
+
+/*    int hour1, minute1;
+    int hour2, minute2;
+    int times;
+    scanf("%d, %d", &hour1, &minute1);
+    scanf("%d, %d", &hour2, &minute2);        
+    times = (hour2 * 60 + minute2) - (hour1 * 60 + minute1);
+    printf("\ntime1 is %d时%d分", hour1, minute1);
+    printf("\ntime2 is %d时%d分", hour2, minute2);
+    printf("\ntimes = %d, 时间间隔为%d:%d\n", times, times/60, times%60);
+    return 0;       
+
+    int i;
+    if (argc != 5)
+    {
+        printf("argc is %d\n", argc);
+        for(i=0; i<argc; i++)
+            printf("argv[%d]= %s \n", i, argv[i]);
+        printf("Error: 请重新输入参数。如：时1 分1 时2 分2");
+        return 1;
+    }
+
+    int hour1, minute1;
+    int hour2, minute2;
+    int times;
+    hour1 = atoi(argv[1]);
+    minute1 = atoi(argv[2]);
+    hour2 = atoi(argv[3]);
+    minute2 = atoi(argv[4]);
+
+    times = (hour2 * 60 + minute2) - (hour1 * 60 + minute1);
+    printf("\ntime1 is %d时%d分", hour1, minute1);
+    printf("\ntime2 is %d时%d分", hour2, minute2);
+    printf("\ntimes = %d, 时间间隔为%d:%d\n", times, times/60, times%60);
+    return 0;
+*/
+}
